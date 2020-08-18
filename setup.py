4c9cc5dd568f360640da57831d63c20d83510c35
Johannes Blaschke <jpblaschke@lbl.gov>
2020-08-17 21:37:04 -0700

    add setup script

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+import setuptools
+
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+
+setuptools.setup(
+    name="callmonitor-johannes.blaschke",
+    version="0.1.0",
+    author="Johannes Blaschke",
+    author_email="johannes@blaschke.science",
+    description="A light-weight package that allows you to monitor function calls with ease.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JBlaschke/call-monitor",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
