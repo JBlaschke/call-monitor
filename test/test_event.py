2a60e8237b57841be75d8040144ae6903e633654
Johannes Blaschke <johannes@blaschke.science>
2021-10-01 22:29:19 -0700

    make test a bit more complex

--- test/test_event.py
+++ test/test_event.py
@@ -8,12 +8,24 @@ from callmonitor.libevent import event_here, start, stop, log, event_log
 
 
 @log
+def inner_function(n):
+    sleep(n)
+
+
+@log
+def outer_function(n1, n2):
+    sleep(n1)
+    inner_function(n2)
+
+
+@log
 def test_inplace_logger(n):
 
     start(f"sleep({n})")
     sleep(n)  # sleep for n seconds
     stop("sleep")
-
+    outer_function(n, 2*n)
+    inner_function(n)
 
 
 if __name__ == "__main__":
